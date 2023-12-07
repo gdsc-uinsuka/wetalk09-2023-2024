06dd09944f21af8303fee0842fa4d4eb5f20d823
fnakhsan <fn.akhsan@gmail.com>
2023-12-07 22:38:34 +0700

    init

---
+++ app/src/main/java/gdsc/suka/wetalk09/RegisterActivity.kt
@@ -0,0 +1,11 @@
+package gdsc.suka.wetalk09
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
